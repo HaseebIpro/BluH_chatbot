0f24a573ddead157743206b88e378b02ec80f096
HaseebIpro <mohammed.haseeb@iprotechs.com>
2024-11-27 12:19:55 +0530

    added requirements.txt

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import streamlit as st
 from utils import handle_user_query
 
-st.title('Ask BluH')
+def main():
+    st.title('Ask BluH')
 
-left, right = st.columns([0.8, 0.2])
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
+    left, right = st.columns([0.8, 0.2])
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
 
-for message in st.session_state.messages:
-    st.chat_message(message['role']).markdown(message['content'])
+    for message in st.session_state.messages:
+        st.chat_message(message['role']).markdown(message['content'])
 
-prompt = st.chat_input('Pass your prompt here')
+    prompt = st.chat_input('Pass your prompt here')
 
-if prompt:
-    st.chat_message('user').markdown(prompt)
-    st.session_state.messages.append({'role':'user', 'content':prompt})
-    response = handle_user_query(prompt, st.session_state.messages)
-    st.chat_message('assistant').markdown(response)
-    st.session_state.messages.append({'role':'assistant', 'content':response})
+    if prompt:
+        st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role':'user', 'content':prompt})
+        response = handle_user_query(prompt, st.session_state.messages)
+        st.chat_message('assistant').markdown(response)
+        st.session_state.messages.append({'role':'assistant', 'content':response})
+
+
+if __name__ == "__main__":
+    main()
